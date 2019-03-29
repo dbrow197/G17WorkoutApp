4b3b447a589162f0f609cdcc92973fe0eeb15e11
dbrow197 <dbrow197@uncc.edu>
2019-03-29 17:59:38 +0000

    homepage controller created, views updated, CSS updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+     get 'tutorial/tutorials'
+  get 'stat/stat'
+  get 'homepage/index'
+  get 'homepage/about'
+  get 'homepage/stats'
+  get 'homepage/tutorial'
+  get 'homepage/progression'
+  
+  resources :workouts
+  
+  root 'homepage#index'
+    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
