c8c8c7e4f573d89c9ca875b743c9ee5824f47226
dbrow197 <dbrow197@uncc.edu>
2019-04-01 21:08:56 +0000

    created daytype controller

---
+++ app/controllers/daytype_controller.rb
@@ -0,0 +1,2 @@
+class DaytypeController < ApplicationController
+end
---
+++ test/controllers/daytype_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DaytypeControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
