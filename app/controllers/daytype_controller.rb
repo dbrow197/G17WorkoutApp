0b9677c4572ecacf5b78d0276b3bcdfb6c2628f6
dbrow197 <dbrow197@uncc.edu>
2019-04-02 20:42:11 +0000

    Creaed crud for daytype controller

--- app/controllers/daytype_controller.rb
+++ app/controllers/daytype_controller.rb
@@ -1,2 +1,19 @@
 class DaytypeController < ApplicationController
+     def index  
+        @user = User.all
+     end
+    
+    def show
+        @user = User.find(params[:id])
+    end
+    def update
+         @user = User.find(params[:id])
+         
+         if @user.update(user_params)
+             
+            redirect_to @article
+         else
+            render 'edit'
+         end
+    end
 end
