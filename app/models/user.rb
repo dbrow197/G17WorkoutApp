a66405adf108f08c051abf42ca7972725582ca30
dbrow197 <dbrow197@uncc.edu>
2019-04-03 14:28:39 +0000

    devise migrated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_one :name
     has_one :email
     has_one :progression
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
      get 'tutorial/tutorials'
   get 'stat/stat'
   get 'homepage/index'
